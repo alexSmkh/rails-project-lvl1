36deb0c3c024f850769353c0093515b68e1e6438
Alexander Samokhvalov <alexshikari9333@gmail.com>
2021-11-21 00:38:56 +0300

    Added the base functionality

--- lib/form_generator.rb
+++ lib/form_generator.rb
@@ -1,7 +1,27 @@
 # frozen_string_literal: true
 
-require_relative "form_generator/version"
+require_relative 'form_generator/version'
 
 module FormGenerator
-  class Error < StandardError; end
+  class Error < StandardError
+  end
+
+  # This class responsible for creating an html tag
+  class Tag
+    class << self
+      def build(tag, attrs = {})
+        tag_attrs = build_attrs(attrs)
+
+        return "<#{tag}#{tag_attrs}>#{yield}</#{tag}>" if block_given?
+
+        "<#{tag}#{tag_attrs}>"
+      end
+
+      private
+
+      def build_attrs(attrs)
+        attrs.map { |key, value| " #{key}=\"#{value}\"" }.join
+      end
+    end
+  end
 end
--- lib/form_generator/version.rb
+++ lib/form_generator/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module FormGenerator
-  VERSION = "0.1.0"
+  VERSION = '0.1.0'
 end

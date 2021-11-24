a68bd2740abcc0f450759736f285f00da4d45748
Alexander Samokhvalov <alexshikari9333@gmail.com>
2021-11-24 15:35:31 +0300

    Removed tests

--- test/form_generator_test.rb
+++ test/form_generator_test.rb
@@ -6,21 +6,4 @@ class FormGeneratorTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::FormGenerator::VERSION
   end
-
-  def setup
-    @user_struct = Struct.new(:name, :job, :about, keyword_init: true)
-  end
-
-  def test_form_for_function
-    expected_form = File.read('test/fixtures/form/form_with_elements.html')
-    user = @user_struct.new(name: 'Frank', job: 'programmer')
-    form = FormGenerator.form_for user, url: '/user' do |f|
-      f.input :name
-      f.input :job
-      f.input :about, as: 'text'
-      f.submit
-    end
-
-    assert_equal expected_form, form
-  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,5 @@
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'form_generator'
-require 'form_generator/input'
-require 'form_generator/label'
-require 'form_generator/form'
-require 'form_generator/textarea'
 
 require 'minitest/autorun'
